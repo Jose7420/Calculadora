bf7b017712844d137e5f7a2573c79e91dbf79bde
Jose7420 <7962446@alu.murciaeduca.es>
2021-03-22 16:27:18 +0100

    prueba

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calculadora;
 
+package calculadora;
+// esto es un comentario desde local
 /**
  *
  * @author redes
